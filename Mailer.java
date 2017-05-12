b85470f0e24516f9e56fe7b288c05a4ece58da11
Asara Senaratne <subuddhiasara@gmail.com>
2017-05-12 13:00:14 +0530

    Add files via upload

---
+++ Mailer.java
@@ -0,0 +1,15 @@
+package mailClient;
+
+import org.apache.log4j.Logger;
+
+public class Mailer {
+	
+	private static Logger logger = Logger.getLogger(Mailer.class);
+	
+		
+	public static void main(String[] args) throws Exception
+	{
+		logger.error("Mail Client ");
+		logger.info("Printed on File");			
+	}
+}
